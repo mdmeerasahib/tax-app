0b76471abc28f4103d51a5862395a77b90ce3689
Mohamed Meerasahib Mowzoon <mdmeerasahib@gmail.com>
2022-03-02 00:23:06 +0800

    citizen-info-service
    Initial Commit

---
+++ citizen-info-service/src/main/java/com/test/citizenservice/CitizenInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.test.citizenservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitizenInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitizenInfoServiceApplication.class, args);
+	}
+
+}

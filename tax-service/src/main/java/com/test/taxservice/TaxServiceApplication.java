b5d56f2e778b4cd055054af5ac334de687066e47
Mohamed Meerasahib Mowzoon <mdmeerasahib@gmail.com>
2022-03-03 00:45:55 +0800

    Add WebClient Beans
    Add WebClient Beans

--- tax-service/src/main/java/com/test/taxservice/TaxServiceApplication.java
+++ tax-service/src/main/java/com/test/taxservice/TaxServiceApplication.java
@@ -1,13 +1,33 @@
 package com.test.taxservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class TaxServiceApplication {
 
+	
+	@Value("${citizen-service.url}")
+	private String citizenServiceUrl;
+	
+	@Value("${address-service.url}")
+	private String addressServiceUrl;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TaxServiceApplication.class, args);
+	}
+	
+	@Bean
+	WebClient citizenServiceWebClient() {
+		return WebClient.builder().baseUrl(citizenServiceUrl).build();
+	}
+	
+	@Bean
+	WebClient addressServiceWebClient() {
+		return WebClient.builder().baseUrl(addressServiceUrl).build();
 	}
 
 }

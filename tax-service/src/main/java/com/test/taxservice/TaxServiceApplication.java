f84a26e3e5b05abe8a976b1165e3aee63a9fceb5
Mohamed Meerasahib Mowzoon <mdmeerasahib@gmail.com>
2022-04-16 04:57:46 +0800

    Remove Unnecessary WebClient References
    Remove Unnecessary WebClient References

--- tax-service/src/main/java/com/test/taxservice/TaxServiceApplication.java
+++ tax-service/src/main/java/com/test/taxservice/TaxServiceApplication.java
@@ -1,12 +1,9 @@
 package com.test.taxservice;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -14,24 +11,9 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class TaxServiceApplication {
 
 	
-	@Value("${citizen-service.url}")
-	private String citizenServiceUrl;
-	
-	@Value("${address-service.url}")
-	private String addressServiceUrl;
-	
 	public static void main(String[] args) {
 		SpringApplication.run(TaxServiceApplication.class, args);
 	}
 	
-	@Bean
-	WebClient citizenServiceWebClient() {
-		return WebClient.builder().baseUrl(citizenServiceUrl).build();
-	}
-	
-	@Bean
-	WebClient addressServiceWebClient() {
-		return WebClient.builder().baseUrl(addressServiceUrl).build();
-	}
 
 }
